dcf8f1e65c73654cbc0d63aea59b1fe8731ec804
jozidestringz-lgtm <jozidestringz@gmail.com>
2026-04-13 21:52:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+emailjs.init("UYtN5fZjMzFRNNvI3");
+
+function show(pageId) {
+    document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
+    document.getElementById(pageId).classList.add("active");
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.send("service_x7uihsa", "template_89us4tp", {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    }).then(() => {
+        document.getElementById("successMsg").innerText = "Message sent 📩";
+        this.reset();
+    }).catch(() => {
+        document.getElementById("successMsg").innerText = "Failed to send";
+    });
+});
