1a9be831329ca0778548609f2f20aa02209a5085
jozidestringz-lgtm <jozidestringz@gmail.com>
2026-04-14 10:56:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-// EMAILJS INIT
-emailjs.init({
-  publicKey: "UYtN5fZjMzFRNNvI3"
-});
-
-// PAGE SWITCH
-function show(pageId) {
-  document.querySelectorAll(".page").forEach(page => {
-    page.classList.remove("active");
-  });
-
-  document.getElementById(pageId).classList.add("active");
+function test() {
+  alert("JS is working 🎉");
 }
-
-// CONTACT FORM
-document.addEventListener("DOMContentLoaded", function () {
-
-  document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    emailjs.send("service_x7uihsa", "template_89us4tp", {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value
-    })
-    .then(() => {
-      document.getElementById("successMsg").innerText = "Message sent successfully!";
-      this.reset();
-    })
-    .catch(() => {
-      document.getElementById("successMsg").innerText = "Failed to send message.";
-    });
-
-  });
-
-});
