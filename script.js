135a47dcf67fde95fd1540221ebc36f03f6c2793
jozidestringz-lgtm <jozidestringz@gmail.com>
2026-04-14 10:33:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
+// EMAILJS INIT
 emailjs.init({
   publicKey: "UYtN5fZjMzFRNNvI3"
 });
 
-function show(pageId){
-document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
-document.getElementById(pageId).classList.add("active");
+// PAGE SWITCH
+function show(pageId) {
+  document.querySelectorAll(".page").forEach(page => {
+    page.classList.remove("active");
+  });
+
+  document.getElementById(pageId).classList.add("active");
 }
 
-document.getElementById("contactForm").addEventListener("submit", function(e){
-e.preventDefault();
+// CONTACT FORM
+document.addEventListener("DOMContentLoaded", function () {
 
-emailjs.send("service_x7uihsa","template_89us4tp",{
-name: document.getElementById("name").value,
-email: document.getElementById("email").value,
-message: document.getElementById("message").value
-})
-.then(()=>{
-document.getElementById("successMsg").innerText="Message sent successfully";
-this.reset();
-})
-.catch(()=>{
-document.getElementById("successMsg").innerText="Failed to send";
-});
+  document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.send("service_x7uihsa", "template_89us4tp", {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    })
+    .then(() => {
+      document.getElementById("successMsg").innerText = "Message sent successfully!";
+      this.reset();
+    })
+    .catch(() => {
+      document.getElementById("successMsg").innerText = "Failed to send message.";
+    });
+
+  });
+
 });
