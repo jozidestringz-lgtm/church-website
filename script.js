21156c5fe6b8ff03476c41ea72627e8697461630
jozidestringz-lgtm <jozidestringz@gmail.com>
2026-04-14 10:28:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-emailjs.init("UYtN5fZjMzFRNNvI3");
+emailjs.init({
+  publicKey: "UYtN5fZjMzFRNNvI3"
+});
 
-function show(pageId) {
-    document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
-    document.getElementById(pageId).classList.add("active");
+function show(pageId){
+document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
+document.getElementById(pageId).classList.add("active");
 }
 
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("contactForm").addEventListener("submit", function(e){
+e.preventDefault();
 
-    emailjs.send("service_x7uihsa", "template_89us4tp", {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    }).then(() => {
-        document.getElementById("successMsg").innerText = "Message sent 📩";
-        this.reset();
-    }).catch(() => {
-        document.getElementById("successMsg").innerText = "Failed to send";
-    });
+emailjs.send("service_x7uihsa","template_89us4tp",{
+name: document.getElementById("name").value,
+email: document.getElementById("email").value,
+message: document.getElementById("message").value
+})
+.then(()=>{
+document.getElementById("successMsg").innerText="Message sent successfully";
+this.reset();
+})
+.catch(()=>{
+document.getElementById("successMsg").innerText="Failed to send";
+});
 });
